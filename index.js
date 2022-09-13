2058b5b2534faa52dbdf85f8ae30056b99d2f45c
Barbara Szewczyk <comamysza@gmail.com>
2022-09-13 21:03:39 +0200

    Adds different get paths and responses

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res) => {
-	console.log('new request came in!');
-	res.send('Got your request, working on it!');
+// app.use((req, res) => {
+// 	console.log('new request came in!');
+// 	res.send('Got your request, working on it!');
+// });
+
+app.get('/', (req, res) => {
+	res.send('Welcome to my first express using homepage!');
+});
+
+app.get('/p/:subpage', (req, res) => {
+	const { subpage } = req.params;
+	res.send(`<h1>Browsing the ${subpage} subpage</h1>`);
+});
+
+app.get('/p/:subpage/:postId', (req, res) => {
+	const { subpage, postId } = req.params;
+	res.send(`<h1>Viewing post ID: ${postId} the ${subpage} subpage</h1>`);
+});
+
+app.get('/dogs', (req, res) => {
+	res.send('WOOF WOOF!');
+});
+
+app.get('/cats', (req, res) => {
+	res.send('MEOOOOOW!');
 });
 
 app.listen(3000, () => {
